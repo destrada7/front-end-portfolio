cd6b30fd82be6da354429a7d4b8344197988af38
David Estrada <113490499+destrada7@users.noreply.github.com>
2024-05-21 12:15:39 -0500

    EmailJS added to the form. Added some style adjustments to the form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import emailjs from 'emailjs-com';
 import '../styles/ContactForm.css';
 
-const ContactForm = ({ onFormSubmit }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+const ContactForm = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
     const onSubmit = (data) => {
-        console.log(data);
-        onFormSubmit(data); // Here you call the function passed as a prop
+        emailjs.send('service_o6zhiwu', 'template_k4o2hkf', data, 'ZkxwIiF71y2_yFS-H')
+            .then((response) => {
+                alert('Form submitted successfully!');
+                reset();
+            })
+            .catch((error) => {
+                alert('An error occurred while submitting the form.');
+                console.error('EmailJS Error:', error);
+            });
     };
 
     return(
